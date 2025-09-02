4cf9d02cf82db39b614f88e09b5da1297b2b77d0
thanikth <68737735+thanikth@users.noreply.github.com>
2025-09-02 22:30:27 +0700

    test: scan qrcode

--- src/components/qrCode/scanQRCode.tsx
+++ src/components/qrCode/scanQRCode.tsx
@@ -1,5 +1,3 @@
-// ...existing code...
-"use client";
 import { useEffect, useRef, useState } from "react";
 import { BrowserQRCodeReader } from "@zxing/browser";
 // import { fetchUserByUserId } from "@/pages/api/users/getUser"; // removed - use fetch to /api instead
