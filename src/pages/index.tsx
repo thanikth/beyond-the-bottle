40a595ca3746b6391e03930f39d5251830e2b938
thanikth <68737735+thanikth@users.noreply.github.com>
2025-09-02 20:55:23 +0700

    test: test qrcode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
+import UserQRCode from "@/components/qrCode/genQRCode";
+import ScanQRCode from "@/components/qrCode/scanQRCode";
 import { Button } from "antd";
 import Link from 'next/link'
+import { useState } from "react";
 
 export default function Home() {
+  const [isOpenCamera, setIsOpenCamera] = useState<boolean>(false);
   return (
     <div style={{ textAlign: "center", marginTop: 100 }}>
       <h1>LINE Points System</h1>
       <Link href="/api/auth/login">
         <button type="button">Login with LINE</button>
       </Link>
+
+      <UserQRCode userId={'U6fc80ecbab35272e6a5cfd54695a4c8b'} />
+
+      <button role="button" onClick={() => {
+              setIsOpenCamera(!isOpenCamera);
+            }}>Scan QRCode</button>
+            {
+              isOpenCamera && (
+                <ScanQRCode />
+              )
+            }
     </div>
   );
 }
